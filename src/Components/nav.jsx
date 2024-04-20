f2fa2221dc9839e84662ecd8a7779795b2b466bc
a04k <63ahmedkhaled@gmail.com>
2024-04-20 02:03:31 +0200

    Rebuilt the nav to address the 'active' class not working bug.

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../assets/stylesheets/nav.scss';
 import Logo from '../assets/Images/logo.svg';
@@ -7,53 +8,36 @@ import News from '../assets/Images/news.svg';
 import Contact from '../assets/Images/contact.svg';
 import Assignments from '../assets/Images/assignments.svg';
 
-
 export default function Nav() {
   const location = useLocation();
-  const currentLocation = location.pathname.toLowerCase();
+  const [currentLocation, setCurrentLocation] = useState('');
+
+  useEffect(() => {
+    setCurrentLocation(location.pathname.toLowerCase());
+  }, [location]);
 
   return (
     <nav>
       <Link to="/">
-        <img className='logo' src= {Logo}></img>
+        <img className='logo' src={Logo} alt="Logo" />
       </Link>
 
       <ul>
-        <li>
-          <img src= {Home} alt="" />
-          <Link to="/" className={currentLocation === '/' ? 'active' : ''} id="home">
-          <span className="link-text">Home</span>
-
-          </Link>
-        </li>
-        <li>
-          <img src={Doc} alt="" />
-          <Link to="/lessons" className={currentLocation === '/lessons' ? 'active' : ''}>
-          <span className="link-text">Lessons</span>
-
-          </Link>
-        </li>
-        <li>
-          <img src={Assignments} alt="" />
-          <Link to="/assignments" className={currentLocation === '/assignments' ? 'active' : ''}>
-          <span className="link-text">Assignments</span>
-
-          </Link>
-        </li>
-        <li>
-          <img src={News} alt="" />
-          <Link to="/news" className={currentLocation === '/news' ? 'active' : ''}>
-            <span className="link-text">News</span>
-          </Link>
-        </li>
-        <li>
-          <img src={Contact} alt="" />
-          <Link to="/contact" className={currentLocation === '/contact' ? 'active' : ''}>
-          <span className="link-text">Ask a question</span>
-          </Link>
-        </li>
+        <NavItem to="/" icon={Home} label="Home" currentLocation={currentLocation} />
+        <NavItem to="/lessons" icon={Doc} label="Lessons" currentLocation={currentLocation} />
+        <NavItem to="/assignments" icon={Assignments} label="Assignments" currentLocation={currentLocation} />
+        <NavItem to="/news" icon={News} label="News" currentLocation={currentLocation} />
+        <NavItem to="/contact" icon={Contact} label="Ask a question" currentLocation={currentLocation} />
       </ul>
     </nav>
   );
 }
 
+function NavItem({ to, icon, label, currentLocation }) {
+  return (
+    <li className={currentLocation === to ? 'active' : ''}>
+      <img src={icon} alt={label} />
+      <Link to={to}>{label}</Link>
+    </li>
+  );
+}
